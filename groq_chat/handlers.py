5e755702f56c6e610b14117a2ea51eb028c54d2c
Artem <bannderovaa@gmail.com>
2025-12-14 13:41:34 +0600

    мелкие правки

--- groq_chat/handlers.py
+++ groq_chat/handlers.py
@@ -255,21 +255,20 @@ async def info_command_handler(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> None:
     """Get info about the bot"""
-    message = f"""**__Conversation Info:__**
-**Model**: `{context.user_data.get("model", await get_default_model())}`
-"""
-
+    message = (
+        f"**__Conversation Info:__**\n"
+        f"**Model**: `{context.user_data.get("model", await get_default_model())}`"
+    )
+    message = await translate(message)
     # if context.user_data.get("system_prompt") is not None:
     #     message += f"\n**System Prompt**: \n```\n{context.user_data.get("system_prompt")}\n```"
-    await update.message.reply_text(format_message(message), parse_mode=ParseMode.HTML)
+    await update.message.reply_text(message, parse_mode=ParseMode.MARKDOWN)
 
 
 async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Log the error and send a telegram message to notify the developer."""
     # Log the error before we do anything else, so we can see it even if something breaks.
-    logger.error(
-        "Exception while handling an update:", exc_info=context.error
-    )
+    logger.error("Exception while handling an update:", exc_info=context.error)
 
     # traceback.format_exception returns the usual python message about an exception, but as a
     # list of strings rather than a single string, so we have to join them together.
